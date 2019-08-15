0b7c66c23365f7987e2f1f3133c45f1f7874c906
Nicholas Burnett <nicholasburnett@datamachines.io>
2019-08-15 12:23:18 -0400

    Simple analytic for client testing

--- python/medifor/v1/examples/example_analytic.py
+++ python/medifor/v1/examples/example_analytic.py
@@ -6,8 +6,7 @@ import os.path
 import sys
 import time
 
-import analytic_pb2
-import analyticservice
+from medifor.v1 import analytic_pb2, analyticservice
 
 logging.basicConfig(level=logging.INFO)
 
@@ -16,38 +15,6 @@ def process_image(req, resp):
 
     time.sleep(2)
     resp.score = 0.5
-
-    # Get image size for the explanation below. Just an example of how to
-    # access actual image data.
-    image_size = 0
-    with open(req.image.uri, "rb") as img:
-        data = img.read(1024)
-        while data:
-            image_size += len(data)
-            data = img.read(1024)
-
-    # Write out a useless "mask", which is really just a text file.
-    # A real mask would be written in the same manner.
-    os.makedirs(req.out_dir, exist_ok=True)
-    out_mask = os.path.join(req.out_dir, "mask_test.txt")
-    with open(out_mask, "wb") as f:
-        f.write("hey there\n".encode('utf-8'))
-
-    # Don't forget to specify where we put things.
-    resp.localization.mask.uri = out_mask
-    resp.localization.mask.type = 'text/plain'
-
-    resp.explanation = 'faking a crop task for image {!r}, size {}'.format(
-        req.image.uri, image_size)
-
-    # Fake crop output. Can specify multiple manipulation types by
-    # appending to the manipulation_type repeated field.
-    resp.manipulation_type.append(analytic_pb2.MANIP_CROP)
-
-    # Note that we don't specify an opt-out type. Any missing field in the
-    # protocol will take the "zero" (or empty) value and can thus be omitted
-    # if that's the right answer. In this case, the default is OPT_OUT_NONE.
-
     logging.info("done")
 
 if __name__ == '__main__':
